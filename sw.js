ef6217e4e74b80e0e3db1a42432016de4353da4d
Chad <worktannoury@gmail.com>
2018-01-11 17:05:38 +0200

    Revised

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ self.addEventListener('install', function(event){
 		);
 });
 
-self.addEventListener('XMLHttpRequest', function(event){
+self.addEventListener('fetch', function(event){
 	event.respondWith(
 		caches.match(event.request).then(function(response){
 			if(response) return response;
